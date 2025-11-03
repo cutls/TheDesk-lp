3c85875cd00b3b41f4c54c86dbe15c1a7d077f42
cutls <p@cutls.dev>
2025-11-03 19:53:16 +0900

    lp

--- src/pages/privacy.tsx
+++ src/pages/privacy.tsx
@@ -16,6 +16,9 @@ export default function Home() {
 			<Box>
 				<Text fontSize={25}>Privacy Policy</Text>
 				<Text>このアプリケーションはサーバーにアクセスするための認証情報を保持しますが、開発元に送信することはありません。</Text>
+				<Text>最新データを取得するために、開発元のサーバーにアクセスし、そのデータを表示しますが、開発元に送信することはありません。IPアドレス等の個人を特定しうる情報は取得・保存していません。</Text>
+				<Text>Spotifyと連携するために、開発元のサーバーにアクセスしますが。あなたのデータやSpotifyの認証情報を保存することはありません。</Text>
+				<Text>開発元はAWS(Amazon Web Services)を利用しこれらのサービスを提供しています。</Text>
 			</Box>
 		</Container>
 	)
